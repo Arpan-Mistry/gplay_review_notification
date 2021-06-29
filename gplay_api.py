749aa351d56a4aa7b549dbdd2c784233022476d8
Arpan-Mistry <arpanmistry8000@gmail.com>
2021-06-29 14:42:40 +0530

    Signed-off-by: Arpan-Mistry <arpanmistry8000@gmail.com>

---
+++ gplay_api.py
@@ -0,0 +1,34 @@
+'''
+No need to run this api.......(this is just for Referance)
+I have Deployed this API on My Azure,Live link of same api: https://gplay-api.azurewebsites.net/key    plz enter pkg_name in place of key
+examples of package names : com.twitter.android OR com.facebook.katana OR com.whatsapp OR  com.phonepe.app ETC.......
+'''
+
+from google_play_scraper import Sort, reviews
+from flask import Flask,jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def info():
+    return f"<h2>please add Package_name at the end of url with /<br>Ex: http://127.0.0.1:5000/com.facebook.katana</h2>"
+
+@app.route('/<string:input>')
+def index(input):
+    pkg_name=input
+    result = reviews(
+        pkg_name,
+        lang='en', # defaults to 'en'
+        country='us', # defaults to 'us'
+        sort=Sort.NEWEST, # defaults to Sort.MOST_RELEVANT
+        count=50, # defaults to 100
+        filter_score_with=None # defaults to None(means all score)
+        )
+    return jsonify(result[0])
+if __name__ =="__main__":
+    app.run(debug=True)
+
+
+
+
+
---
+++ mainapp.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, request,jsonify
+from multiprocessing import Process
+import smtplib
+import requests,json,re
+
+
+f=open('config.json','r')
+params = json.load(f)["params"]
+
+
+app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__,template_folder='./templates',static_folder='./static')
+app = Flask(__name__)
+
+#------smtp mail configuration--------------
+mail_user = params["user"]
+mail_pswd = params["password"]
+
+#--------simple function--------------------
+def registered(pkg_name,user_name,user_key,user_email):
+    #----empty list of sent mails & initializing some counter Variables-----
+    sent_revs=[]
+    total_count=0
+    neg_count=0
+    neg_per=0
+    while(True):
+        #------------------deployed (gplay_api.py) on my azure so using that link (you can refer gplay_api.py file)
+        url='https://gplay-api.azurewebsites.net/'+pkg_name
+        r = requests.get(url).text
+        json_obj = json.loads(r)
+        # print(json_obj)  
+        
+        #---------fetching reviews one by one from json & comparing with Topic registerd by user
+        for element in json_obj:
+            review = element["content"]
+            # print(review)
+            if re.search(user_key.lower(),review.lower()):
+                if review not in sent_revs:
+                    print("MATCHING : "+review)
+                    
+                    total_count+=1
+                    if element["score"]<3:
+                        neg_count+=1
+                    
+                    #----------------copying old negativity percentage
+                    temp=neg_per    
+                    
+                    neg_per=((neg_count*100)/total_count)
+                    neg_per=round(neg_per, 2) 
+                    
+                    per_diff= neg_per-temp
+                    per_diff=round(per_diff, 2)
+                    
+                    #---------------Settingup message Format
+                    msg="Username : "+element["userName"]+"\nReview : {}\nPosted at : ".format(element["content"])+element["at"]+"\nstars : " +str(element["score"])+" out of 5\n---------- STATISTICS --------- \nPercentage of negative reviews : "+str(neg_per)+"%\nPercentage Differnce : "+str(per_diff)+"%"
+                    SUBJECT='Hello ,'+user_name+'!  Notification for new Review on Google Play related with topic : '+ user_key
+                    msg = 'Subject: {}\n\n{}'.format(SUBJECT,msg)
+                    msg=(msg).encode('utf-8')
+                    
+                    #---------------SMTP gmail login & sending mail to Registered User
+                    server = smtplib.SMTP('smtp.gmail.com:587')
+                    server.ehlo()
+                    server.starttls()
+                    server.login(mail_user,mail_pswd)
+                    server.sendmail(mail_user,user_email,msg)
+                    #--------------marking as sent to avoid repeating of same review-------
+                    sent_revs.append(review)
+
+@app.route('/', methods = ['POST','GET'])
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+    if request.method == "POST":
+        pkg_name = request.form['pkg_name']
+        user_email = request.form['email']
+        user_name = request.form['name']
+        user_key = request.form['key']
+        
+        #-----------Using Multiprocessing to Render template & to Run infinite Loop function at same time----------
+        p1=Process(target = registered,args= (pkg_name,user_name,user_key,user_email,)).start()
+        return render_template("done.html")
+
+                                        
+
+if __name__ =="__main__":
+    app.run(debug=True)
